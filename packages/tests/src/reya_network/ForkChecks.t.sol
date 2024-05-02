c83f79b96dd0a8dd397f0ffb6e9e06f5c4dfe414
Alexandru Niculae <niculae.alexandru1@gmail.com>
2024-05-02 14:08:57 +0100

    feat: monorepo

--- packages/tests/src/reya_network/ForkChecks.t.sol
+++ packages/tests/src/reya_network/ForkChecks.t.sol
@@ -1,31 +1,30 @@
 pragma solidity >=0.8.19 <0.9.0;
 
 import "forge-std/Test.sol";
-import { IERC20TokenModule } from "@voltz-protocol/token/src/interfaces/IERC20TokenModule.sol";
-import { OwnerUpgradeModule } from "@voltz-protocol/util-modules/src/modules/OwnerUpgradeModule.sol";
-import { ISocketExecutionHelper } from "@voltz-protocol/periphery/src/interfaces/external/ISocketExecutionHelper.sol";
-import { DepositPassivePoolInputs } from "@voltz-protocol/periphery/src/libraries/DataTypes.sol";
-import { PassivePoolProxy } from "./proxies/PassivePool.sol";
-import { PeripheryRouter } from "./proxies/Periphery.sol";
+import { IERC20TokenModule } from "../interfaces/IERC20TokenModule.sol";
+import { IOwnerUpgradeModule } from "../interfaces/IOwnerUpgradeModule.sol";
+import { ISocketExecutionHelper } from "../interfaces/ISocketExecutionHelper.sol";
+import { IPeripheryProxy, DepositPassivePoolInputs } from "../interfaces/IPeripheryProxy.sol";
+import { IPassivePoolProxy } from "../interfaces/IPassivePoolProxy.sol";
 
 contract ForkChecks is Test {
     string REYA_RPC = "https://rpc.reya.network";
 
     address multisig = 0x1Fe50318e5E3165742eDC9c4a15d997bDB935Eb9;
 
-    address payable public core = payable(0xA763B6a5E09378434406C003daE6487FbbDc1a80);
-    address payable public pool = payable(0xB4B77d6180cc14472A9a7BDFF01cc2459368D413);
-    address payable public perp = payable(0x27E5cb712334e101B3c232eB0Be198baaa595F5F);
-    address public oracleManager = 0xC67316Ed17E0C793041CFE12F674af250a294aab;
-    address payable public periphery = payable(0xCd2869d1eb1BC8991Bc55de9E9B779e912faF736);
-    address public exchangePass = 0x76e3f2667aC55d502e26e59C5A6B46e7079217c7;
-    address public accountNft = 0x0354e71e0444d08e0Ce5E49EB91531A1Cac61144;
+    address payable core = payable(0xA763B6a5E09378434406C003daE6487FbbDc1a80);
+    address payable pool = payable(0xB4B77d6180cc14472A9a7BDFF01cc2459368D413);
+    address payable perp = payable(0x27E5cb712334e101B3c232eB0Be198baaa595F5F);
+    address oracleManager = 0xC67316Ed17E0C793041CFE12F674af250a294aab;
+    address payable periphery = payable(0xCd2869d1eb1BC8991Bc55de9E9B779e912faF736);
+    address exchangePass = 0x76e3f2667aC55d502e26e59C5A6B46e7079217c7;
+    address accountNft = 0x0354e71e0444d08e0Ce5E49EB91531A1Cac61144;
 
-    address public usd = 0xa9F32a851B1800742e47725DA54a09A7Ef2556A3;
-    address public usdc = 0x3B860c0b53f2e8bd5264AA7c3451d41263C933F2;
+    address rusd = 0xa9F32a851B1800742e47725DA54a09A7Ef2556A3;
+    address usdc = 0x3B860c0b53f2e8bd5264AA7c3451d41263C933F2;
 
-    address public socketUsdcExecutionHelper = 0x9ca48cAF8AD2B081a0b633d6FCD803076F719fEa;
-    address public socketUsdcController = 0x1d43076909Ca139BFaC4EbB7194518bE3638fc76;
+    address socketUsdcExecutionHelper = 0x9ca48cAF8AD2B081a0b633d6FCD803076F719fEa;
+    address socketUsdcController = 0x1d43076909Ca139BFaC4EbB7194518bE3638fc76;
 
     constructor() {
         try vm.activeFork() { }
@@ -81,27 +80,28 @@ contract ForkChecks is Test {
         for (uint256 i = 0; i < proxies.length; i += 1) {
             address proxy = proxies[i];
 
-            assertEq(OwnerUpgradeModule(proxy).owner(), multisig);
+            assertEq(IOwnerUpgradeModule(proxy).owner(), multisig);
 
             vm.prank(attacker);
             vm.expectRevert();
-            OwnerUpgradeModule(proxy).upgradeTo(ownerUpgradeModule);
+            IOwnerUpgradeModule(proxy).upgradeTo(ownerUpgradeModule);
 
             vm.prank(multisig);
-            OwnerUpgradeModule(proxy).upgradeTo(ownerUpgradeModule);
+            IOwnerUpgradeModule(proxy).upgradeTo(ownerUpgradeModule);
         }
 
-        assertEq(OwnerUpgradeModule(accountNft).owner(), core);
+        assertEq(IOwnerUpgradeModule(accountNft).owner(), core);
     }
 
     function testFuzz_PoolDepositWithdraw(address attacker) public {
-        uint128 poolId = 1;
-
-        uint256 attackerSharesAmount = PassivePoolProxy(pool).getAccountBalance(poolId, attacker);
-        vm.assume(attackerSharesAmount == 0);
-
         address user = vm.addr(1);
+        vm.assume(attacker != user);
+
+        uint128 poolId = 1;
         uint256 amount = 100e6;
+
+        uint256 attackerSharesAmount = IPassivePoolProxy(pool).getAccountBalance(poolId, attacker);
+        vm.assume(attackerSharesAmount == 0);
 
         deal(usdc, periphery, amount);
 
@@ -110,17 +110,16 @@ contract ForkChecks is Test {
         vm.mockCall(
             socketUsdcExecutionHelper, abi.encodeCall(ISocketExecutionHelper.bridgeAmount, ()), abi.encode(amount)
         );
-        PeripheryRouter(periphery).depositPassivePool(inputs);
+        IPeripheryProxy(periphery).depositPassivePool(inputs);
 
-        uint256 userSharesAmount = PassivePoolProxy(pool).getAccountBalance(poolId, user);
+        uint256 userSharesAmount = IPassivePoolProxy(pool).getAccountBalance(poolId, user);
         assert(userSharesAmount > 0);
 
         vm.prank(attacker);
         vm.expectRevert();
-        PassivePoolProxy(pool).removeLiquidity(poolId, userSharesAmount, 0);
+        IPassivePoolProxy(pool).removeLiquidity(poolId, userSharesAmount, 0);
 
-        // withdrawals are blocked at the moment
         vm.prank(user);
-        PassivePoolProxy(pool).removeLiquidity(poolId, userSharesAmount, 0);
+        IPassivePoolProxy(pool).removeLiquidity(poolId, userSharesAmount, 0);
     }
 }
