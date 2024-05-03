303b6ca9da900af0e3dffe7fff454d7a0959eb10
Alexandru Niculae <niculae.alexandru1@gmail.com>
2024-05-03 15:15:51 +0100

    bump cannon

--- packages/tests/src/reya_network/ForkChecks.t.sol
+++ packages/tests/src/reya_network/ForkChecks.t.sol
@@ -120,6 +120,7 @@ contract ForkChecks is Test {
         IPassivePoolProxy(pool).removeLiquidity(poolId, userSharesAmount, 0);
 
         vm.prank(user);
+        vm.expectRevert();
         IPassivePoolProxy(pool).removeLiquidity(poolId, userSharesAmount, 0);
     }
 }
