a80c220492592fce44e088099f4cb6d1e331f850
Artur Begyan <arturbegyan98@gmail.com>
2024-05-26 12:32:54 +0100

    feat: bump delta

--- packages/tests/test/reya_network/trade/FundingRate.fork.t.sol
+++ packages/tests/test/reya_network/trade/FundingRate.fork.t.sol
@@ -44,7 +44,7 @@ contract FundingRateForkTest is ReyaForkTest {
         int256 ethFundingRate2 = IPassivePerpProxy(perp).getLatestFundingRate(1);
         int256 btcFundingRate2 = IPassivePerpProxy(perp).getLatestFundingRate(2);
 
-        assertApproxEqAbsDecimal(ethFundingRate2 - ethFundingRate1, ethPSlippage.unwrap() * 1.0e18 / 1e18, 1e12, 18);
-        assertApproxEqAbsDecimal(btcFundingRate2 - btcFundingRate1, btcPSlippage.unwrap() * 1.0e18 / 1e18, 1e12, 18);
+        assertApproxEqAbsDecimal(ethFundingRate2 - ethFundingRate1, ethPSlippage.unwrap() * 1.0e18 / 1e18, 1e13, 18);
+        assertApproxEqAbsDecimal(btcFundingRate2 - btcFundingRate1, btcPSlippage.unwrap() * 1.0e18 / 1e18, 1e13, 18);
     }
 }
