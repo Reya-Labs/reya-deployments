2fb818777ce8c1657c421e6406d21565d3d98906
Alexandru Niculae <niculae.alexandru1@gmail.com>
2024-07-04 16:05:52 +0100

    feat: adjust tests

--- packages/tests/test/reya_common/BaseReyaForkTest.sol
+++ packages/tests/test/reya_common/BaseReyaForkTest.sol
@@ -70,14 +70,7 @@ contract BaseReyaForkTest is StorageReyaForkTest {
         MarketConfigurationData memory marketConfig = IPassivePerpProxy(sec.perp).getMarketConfiguration(marketId);
         NodeOutput.Data memory marketNodeOutput =
             IOracleManagerProxy(sec.oracleManager).process(marketConfig.oracleNodeId);
-        return ud(marketNodeOutput.price);
-    }
-
-    function getMarketSpotPrice(uint128 marketId, bool roundUp) internal view returns (UD60x18 marketSpotPrice) {
-        MarketConfigurationData memory marketConfig = IPassivePerpProxy(sec.perp).getMarketConfiguration(marketId);
-        NodeOutput.Data memory marketNodeOutput =
-            IOracleManagerProxy(sec.oracleManager).process(marketConfig.oracleNodeId);
-        return roundPrice(ud(marketNodeOutput.price), ud(marketConfig.priceSpacing), roundUp);
+        return roundPrice(ud(marketNodeOutput.price), ud(marketConfig.priceSpacing), false);
     }
 
     function getPriceLimit(SD59x18 base) internal pure returns (UD60x18 priceLimit) {
@@ -157,7 +150,7 @@ contract BaseReyaForkTest is StorageReyaForkTest {
         (s.outputs,) = ICoreProxy(sec.core).execute(accountId, commands);
 
         orderPrice = UD60x18.wrap(abi.decode(s.outputs[0], (uint256)));
-        pSlippage = orderPrice.div(getMarketSpotPrice(marketId, base.gt(ZERO_sd))).intoSD59x18().sub(UNIT_sd);
+        pSlippage = orderPrice.div(getMarketSpotPrice(marketId)).intoSD59x18().sub(UNIT_sd);
     }
 
     function exposureToBase(uint128 marketId, SD59x18 exposure) internal view returns (SD59x18) {
