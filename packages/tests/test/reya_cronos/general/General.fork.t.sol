18267ced9d0e9e1752c58f14aa64c3bc87d3ccde
Artur Begyan <36239705+arturbeg@users.noreply.github.com>
2024-07-13 17:14:14 +0100

    feat: setup oracle adapters deployments  (#54)
    * feat: setup oracle adapters deployer
    
    * feat: stork verifier settings
    
    * feat: update omnibus
    
    * feat: linting
    
    * fix
    
    * feat: reference packages
    
    * fix: comment
    
    * feat: add proxy
    
    * feat: readuy
    
    * fix: lint
    
    * feat: setup stork configs oracle manager
    
    * feat: adjust
    
    * update reducer configs for stork
    
    * feat: introduce sol usdc fallback
    
    * feat: configurations oracle manager
    
    * feat: adjust omni
    
    * feat: update sol market setting
    
    * feat: bumped versions and deployed adapter
    
    * feat: bugs fix + minimal fork check
    
    * feat: avoiding mainnet for now
    
    * feat: set publishers
    
    * feat: bump oracle manager
    
    * Feat/oracle updaters mainnet stork (#56)
    
    * feat: oracle updaters mainnet stork scheleton
    
    * feat: versions
    
    * feat: fork check + oracle adapter mainnet
    
    * feat: no oracle manager
    
    * feat: mainnet publishers
    
    * fix: comments
    
    ---------
    
    Co-authored-by: tudor47 <tudorcostin7@gmail.com>
    Co-authored-by: Costin-Razvan Tudor <39092730+0xZenus@users.noreply.github.com>

--- packages/tests/test/reya_cronos/general/General.fork.t.sol
+++ packages/tests/test/reya_cronos/general/General.fork.t.sol
@@ -68,5 +68,21 @@ contract GeneralForkTest is ReyaForkTest, GeneralForkCheck {
 
         assertLe(usdcUsdNodeOutput.timestamp, block.timestamp);
         assertApproxEqAbsDecimal(usdcUsdNodeOutput.price, 1e18, 0.01e18, 18);
+
+        // todo: move these oracle node IDs to ecosystem config
+        bytes32 solUsdcStorkFallbackNodeId = bytes32(0xbc20afac3a933b7c7f814fc8fe08344d5dabe3d154b0584461b677b8568553c8);
+        NodeOutput.Data memory solUsdcStorkFallbackNodeOutput =
+            IOracleManagerProxy(sec.oracleManager).process(solUsdcStorkFallbackNodeId);
+        assertApproxEqAbsDecimal(solUsdcStorkFallbackNodeOutput.price, 150e18, 100e18, 18);
+
+        bytes32 solUsdcStorkNodeId = bytes32(0xa456e0f61bd6068a3a73176ab1c58b840e57e0a272ca29974d3f0bd709fc96c4);
+        NodeOutput.Data memory solUsdcStorkNodeOutput =
+            IOracleManagerProxy(sec.oracleManager).process(solUsdcStorkNodeId);
+        assertApproxEqAbsDecimal(solUsdcStorkNodeOutput.price, 150e18, 100e18, 18);
+
+        bytes32 usdcUsdStorkNodeId = bytes32(0x28c79729ca502a5cd2565613c087a3bda098a1c78e3f3f45733d03c3482f099d);
+        NodeOutput.Data memory usdcUsdStorkNodeOutput =
+            IOracleManagerProxy(sec.oracleManager).process(usdcUsdStorkNodeId);
+        assertApproxEqAbsDecimal(usdcUsdStorkNodeOutput.price, 1e18, 0.01e18, 18);
     }
 }
---
+++ packages/tests/test/reya_cronos/oracle_manager/OracleAdapter.fork.t.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.8.19 <0.9.0;
+
+import { ReyaForkTest } from "../ReyaForkTest.sol";
+import { OracleAdapterForkCheck } from "../../reya_common/oracle_manager/OracleAdapter.fork.c.sol";
+
+contract OracleAdapterForkTest is ReyaForkTest, OracleAdapterForkCheck {
+    function test_Cronos_fulfillOracleQuery_StorkOracleAdapter() public {
+        check_fulfillOracleQuery_StorkOracleAdapter();
+    }
+}
