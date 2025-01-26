3f068078298cc3413065798d0e320bcd5285b59d
Alexandru Niculae <niculae.alexandru1@gmail.com>
2025-01-26 15:19:12 +0200

    feat: more cleanup

--- packages/tests/test/reya_common/general/General.fork.c.sol
+++ packages/tests/test/reya_common/general/General.fork.c.sol
@@ -883,13 +883,6 @@ contract GeneralForkCheck is BaseReyaForkTest {
                 assertLe(block.timestamp - max_stale_duration, nodeOutput.timestamp);
                 assertEq(nodeDefinition.maxStaleDuration, max_stale_duration);
             }
-
-            // if redstone node, check that the owner of the price feed is the multisig
-            if (nodeDefinition.nodeType == 2) {
-                (address priceFeed,) = abi.decode(nodeDefinition.parameters, (address, uint256));
-                address owner = IOwnerUpgradeModule(priceFeed).owner();
-                assertEq(owner, sec.multisig);
-            }
         }
     }
 
