01bb183682930787751e0d3197f99d761a31a29c
tudor47 <tudorcostin7@gmail.com>
2025-06-11 18:50:08 +0300

    feat: refactored collateral tests

--- packages/tests/test/reya_network/collaterals/WethCollateral.fork.t.sol
+++ packages/tests/test/reya_network/collaterals/WethCollateral.fork.t.sol
@@ -5,27 +5,6 @@ import { WethCollateralForkCheck } from "../../reya_common/collaterals/WethColla
 import "../../reya_common/DataTypes.sol";
 
 contract WethCollateralForkTest is ReyaForkTest, WethCollateralForkCheck {
-    function testFuzz_WETHMintBurn(address attacker) public {
-        vm.assume(attacker != dec.socketController[sec.weth]);
-        checkFuzz_WETHMintBurn(attacker);
-    }
-
-    function test_weth_view_functions() public {
-        check_weth_view_functions();
-    }
-
-    function test_weth_cap_exceeded() public {
-        check_weth_cap_exceeded();
-    }
-
-    function test_weth_deposit_withdraw() public {
-        check_weth_deposit_withdraw();
-    }
-
-    function test_trade_wethCollateral_depositWithdraw() public {
-        check_trade_wethCollateral_depositWithdraw();
-    }
-
     function test_WethTradeWithWethCollateral() public {
         check_WethTradeWithWethCollateral();
     }
