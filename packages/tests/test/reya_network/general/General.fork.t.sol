42b9e32ea9afb8879611ab9fb183c5c1760186df
tudor47 <tudorcostin7@gmail.com>
2025-10-03 19:55:52 +0300

    feat: gas profiling

--- packages/tests/test/reya_network/general/General.fork.t.sol
+++ packages/tests/test/reya_network/general/General.fork.t.sol
@@ -5,9 +5,14 @@ import { GeneralForkCheck } from "../../reya_common/general/General.fork.c.sol";
 
 import "../../reya_common/DataTypes.sol";
 import { IPeripheryProxy, GlobalConfiguration } from "../../../src/interfaces/IPeripheryProxy.sol";
-import { IOracleManagerProxy, NodeOutput, NodeDefinition } from "../../../src/interfaces/IOracleManagerProxy.sol";
+import { IPassivePoolProxy } from "../../../src/interfaces/IPassivePoolProxy.sol";
+import { IOracleManagerProxy, NodeOutput, NodeDefinition, NodeDefinition } from "../../../src/interfaces/IOracleManagerProxy.sol";
 import { IOracleAdaptersProxy, StorkPricePayload } from "../../../src/interfaces/IOracleAdaptersProxy.sol";
 import { IAggregatorV3Interface } from "../../../src/interfaces/IAggregatorV3Interface.sol";
+import { ICoreProxy, CollateralConfig, ParentCollateralConfig, CachedCollateralConfig } from "../../../src/interfaces/ICoreProxy.sol";
+import { SD59x18 } from "@prb/math/SD59x18.sol";
+import { UD60x18 } from "@prb/math/UD60x18.sol";
+import { console2 } from "forge-std/console2.sol";
 
 contract GeneralForkTest is ReyaForkTest, GeneralForkCheck {
     function testFuzz_ProxiesOwnerAndUpgrades(address attacker) public {
@@ -71,5 +76,23 @@ contract GeneralForkTest is ReyaForkTest, GeneralForkCheck {
 
     function test_PeripherySrusdBalance() public view {
         check_periphery_srusd_balance();
+    }
+
+    function test_gas_trade() public {
+        uint256 srUSDPrice = 1085646432105633776;
+        bytes32 nodeId = IOracleManagerProxy(sec.oracleManager).registerNode(3, abi.encode(srUSDPrice), new bytes32[](0));
+        
+        (CollateralConfig memory collateralConfig, ParentCollateralConfig memory parentConfig,) = ICoreProxy(sec.core).getCollateralConfig(1, sec.srusd);
+        parentConfig.oracleNodeId = nodeId;
+        vm.prank(sec.multisig);
+        ICoreProxy(sec.core).setCollateralConfig(1, sec.srusd, collateralConfig, parentConfig);
+
+        executeCoreMatchOrder({
+            marketId: 1,
+            sender: 0xaE173a960084903b1d278Ff9E3A81DeD82275556,
+            base: SD59x18.wrap(-1e16),
+            priceLimit: UD60x18.wrap(0),
+            accountId: 50126
+        });
     }
 }
